cf9ff45c9fd5459043dbfd5807405bd49eb22581
Gakiri001 <gakirijulius@gmail.com>
2024-09-16 07:32:01 +0300

    header styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import "./assets/global.css"
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
      <Header/>
-    </>
+    </BrowserRouter>
   )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import logo from "../../assets/mut_logo.png"
+import { Link } from 'react-router-dom'
+import "./Header.css"
 
 function Header() {
   return (
     <>
-    <div>
-      <div>
+    <div className='Header'>
+      <div className='HeaderImage'>
         <img src={logo} alt="" />
       </div>
-    <ul>
-      <li>Tech News</li>
-      <li>Events</li>
-      <li>Performance</li>
+    <ul className='HeaderLinks'>
+      <li><Link className='links' to="/">Tech News</Link></li>
+      <li><Link className='links' to="/">Events</Link></li>
+      <li><Link className='links' to="/">Performance</Link></li>
+      <li><Link className='links' to="/">Resources</Link></li>
     </ul>
-    <div>
+    <div className='HeaderSign'>
       <button>Sign up</button>
       <button>Sign in</button>
     </div>
