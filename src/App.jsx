3f0203eb9fa81621c51b20af8b49581a1ea37640
Gakiri001 <gakirijulius@gmail.com>
2024-09-17 07:51:24 +0300

    hamburger and responsive header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import Header from './components/Header/Header'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import "./assets/global.css"
-
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./assets/global.css";
 
 function App() {
   return (
     <BrowserRouter>
-     <Header/>
+      <Header />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,63 @@
-import React from 'react'
-import logo from "../../assets/mut_logo.png"
-import { Link } from 'react-router-dom'
-import "./Header.css"
+import React, { useState } from "react";
+import logo from "../../assets/mut_logo.png";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { IoCloseCircleOutline } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setMenuOpen(true);
+  };
+
+  const handleMenuClose = () => {
+    setMenuOpen(false);
+  };
   return (
     <>
-    <div className='Header'>
-      <div className='HeaderImage'>
-        <img src={logo} alt="" />
+      <div className="Header">
+        <div className="HeaderImage">
+          <img src={logo} alt="" />
+        </div>
+        <div className={`HeaderRight ${menuOpen ? "active" : ""}`}>
+          <ul className="HeaderLinks">
+            <div id="menu-close" className="close" onClick={handleMenuClose}>
+              <IoCloseCircleOutline />
+            </div>
+            <li>
+              <Link className="links" to="/">
+                Tech News
+              </Link>
+            </li>
+            <li>
+              <Link className="links" to="/">
+                Events
+              </Link>
+            </li>
+            <li>
+              <Link className="links" to="/">
+                Performance
+              </Link>
+            </li>
+            <li>
+              <Link className="links" to="/">
+                Resources
+              </Link>
+            </li>
+          </ul>
+          <div className="HeaderSign">
+            <button>Sign up</button>
+            <button>Sign in</button>
+          </div>
+        </div>
+        <div id="menu-btn" className="hamburger" onClick={handleMenuOpen}>
+            <GiHamburgerMenu />
+          </div>
       </div>
-    <ul className='HeaderLinks'>
-      <li><Link className='links' to="/">Tech News</Link></li>
-      <li><Link className='links' to="/">Events</Link></li>
-      <li><Link className='links' to="/">Performance</Link></li>
-      <li><Link className='links' to="/">Resources</Link></li>
-    </ul>
-    <div className='HeaderSign'>
-      <button>Sign up</button>
-      <button>Sign in</button>
-    </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
