43dd7721d0b5dffd18a8c6fae1056481e8d44fbc
Gakiri001 <gakirijulius@gmail.com>
2024-09-27 19:05:28 +0300

    created the event post route

--- server/controllers/event.controllers.js
+++ server/controllers/event.controllers.js
@@ -5,6 +5,27 @@ const prisma = new PrismaClient();
 export const createEvent = async (req,res) => {
   try{
     const{title,description, trainer, date, time, venue} = req.body
+
+    const event = await prisma.event.create({
+      data: {
+        title: title,
+        description: description,
+        trainer: trainer,
+        date: new Date (date),
+        time: time,
+        venue: venue, 
+      },
+      select:{
+        id:true,
+        title:true,
+        description:true,
+        trainer:true,
+        date: true,
+        time: true,
+        venue: true, 
+      },
+    })
+    res.status(201).json({success:true, message: "event created successfully"})
   }
   catch(err){
     res.status(500).json({success:false, message:"Server failed"})
---
+++ server/prisma/migrations/20240927153225_event_migration2/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "event_table" ALTER COLUMN "time" SET DATA TYPE TEXT;
